81098f8e041cdf318d15b6085a735c2fb6376890
nikita-malevanec <nikitamalevanec@yahoo.com>
2025-10-22 20:13:33 +0200

    Home task 16

---
+++ lesson_16/home/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+# def home(request):
+#     return HttpResponse("Ласкаво просимо на головну сторінку")
+
+
+def about(request):
+    return HttpResponse("Сторінка про нас")
+
+
+def contact(request):
+    return HttpResponse("Зв'яжіться з нами")
+
+def post_view(request, id):
+    return HttpResponse(f"Ви переглядаєте пост з ID: {id}")
+
+def profile_view(request, username):
+    return HttpResponse(f"Ви переглядаєте профіль користувача: {username}")
+
+def event_view(request, year, month, day):
+    return HttpResponse(f"Дата події: {year}-{month}-{day}")
